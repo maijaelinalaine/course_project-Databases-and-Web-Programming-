607831e74189313d769324bf5889fe405432fb14
maijaelinalaine <maijaelina05@gmail.com>
2025-03-29 11:51:44 +0200

    Add registration/login/logout

--- app.py
+++ app.py
@@ -12,8 +12,27 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+
+    if password1 != password2:
+        return "VIRHE: salasanat eivät ole samat"
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "VIRHE: tunnus on jo varattu"
+
+    return "Tunnus luotu"
 
 @app.route("/login", methods=["GET","POST"])
 def login():
@@ -36,4 +55,4 @@ def login():
 @app.route("/logout")
 def logout():
     del session["username"]
-    return redirect("/")
+    return redirect("/")@app.route("/create", methods=["POST"])
